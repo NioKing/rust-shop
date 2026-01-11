a80091a8e732e2f8fd39bccf37aaf63a50704983
Kirill <kirill508@gmail.com>
2026-01-11 03:06:01 +0300

    added basic chat

--- src/chat/handlers.rs
+++ src/chat/handlers.rs
@@ -14,30 +14,34 @@ use axum::{
     response::Response,
 };
 use axum_extra::{TypedHeader, headers};
-use futures_util::StreamExt;
+use futures_util::{SinkExt, StreamExt};
 
-pub async fn handler(
-    ws: WebSocketUpgrade,
-    State(state): State<crate::utils::types::AppState>,
-) -> Response {
-    ws.on_upgrade(move |socket| handle_socket(socket, state))
+pub async fn handler(ws: WebSocketUpgrade, State(state): State<AppState>) -> Response {
+    ws.on_upgrade(|socket| handle_socket(socket, state))
 }
 
 async fn handle_socket(mut socket: WebSocket, state: AppState) {
-    // let (mut sender, mut receiver) = socket.split();
+    let (mut sender, mut receiver) = socket.split();
 
-    while let Some(msg) = socket.recv().await {
-        let msg = if let Ok(msg) = msg {
-            println!("message: {:?}", msg.to_text().unwrap());
-            msg
-        } else {
-            // client disconnected
-            return;
-        };
+    let mut rx = state.tx.subscribe();
 
-        if socket.send(msg).await.is_err() {
-            // client disconnected
-            return;
+    let mut send_task = tokio::spawn(async move {
+        while let Ok(msg) = rx.recv().await {
+            if sender.send(Message::Text(msg.into())).await.is_err() {
+                break;
+            }
         }
-    }
+    });
+
+    let tx = state.tx.clone();
+    let mut recv_task = tokio::spawn(async move {
+        while let Some(Ok(Message::Text(text))) = receiver.next().await {
+            let _ = tx.send(text.to_string());
+        }
+    });
+
+    tokio::select! {
+        _ = &mut send_task => recv_task.abort(),
+        _ = &mut recv_task => send_task.abort()
+    };
 }
